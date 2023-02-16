e95efa517b3ca646d595fd706c05ee316adb64cc
chlebon <lukas.franek@gitgut.ai>
2023-02-16 17:29:52 +0100

    add boostrap, start data table component

---
+++ client/src/DataTable.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button, Row, Col } from 'react-bootstrap';
+
+const DataTable: React.FC = () => {
+  return (
+    <div className="DataTable">
+       <Row className="HeaderButton">
+        <Col>
+          {/* <Button onClick={} variant='primary' size='lg'>Scan Sreality Flats</Button> */}
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default DataTable;
